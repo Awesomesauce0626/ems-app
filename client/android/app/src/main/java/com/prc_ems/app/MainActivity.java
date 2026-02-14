d1ce8b7d2e3177662565b3e6f617b1ad500e36c6
Awesomesauce0626 <awesomesauce0626@gmail.com>
2026-02-14 16:03:27 +0800

    feat: Implement camera capture and all recent enhancements

--- client/android/app/src/main/java/com/prc_ems/app/MainActivity.java
+++ client/android/app/src/main/java/com/prc_ems/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.prc_ems.app;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(LocationServicePlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
