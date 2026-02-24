b02eef99d3076a80e8b3b49ee2bf678b52ef295e
Awesomesauce0626 <i.am.sam052408@gmail.com>
2026-02-24 19:37:37 +0800

    fix(android): Enable DND bypass for critical alerts

--- client/android/app/src/main/java/com/prc_ems/app/MainActivity.java
+++ client/android/app/src/main/java/com/prc_ems/app/MainActivity.java
@@ -15,29 +15,27 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "EMS Alerts";
             String description = "Channel for critical EMS alerts";
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel("ems_alerts", name, importance);
             channel.setDescription(description);
-            // Configure the channel for high importance
+            
+            // Configure the channel for high importance and to bypass DND
             channel.enableLights(true);
             channel.setLightColor(Color.RED);
             channel.enableVibration(true);
+            channel.setBypassDnd(true); // CRITICAL: This allows sound in silent mode.
             channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
 
             AudioAttributes attributes = new AudioAttributes.Builder()
                     .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
                     .build();
-
+            
             Uri soundUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.alarm);
             channel.setSound(soundUri, attributes);
 
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
